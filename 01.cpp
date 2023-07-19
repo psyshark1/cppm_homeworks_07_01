02ce697ea68d91833bff5648eb23ba96318e2175
Andrey <andrey.zalozhenkov@pikta.ru>
2023-07-19 15:52:58 +0400

    Добавьте файлы проекта.

---
+++ 01.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+#define MODE 1
+
+#ifndef MODE
+#error MODE not defined!
+#endif
+
+#if MODE == 1
+short add(const short& a, const short& b);
+#endif
+
+int main(int argc, char** argv)
+{
+    std::system("chcp 1251");
+
+#if MODE == 0
+    std::cout << "Работаю в тестовом режиме" << std::endl;
+#elif MODE == 1
+    std::cout << "Работаю в боевом режиме" << std::endl;
+#else
+    std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+    std::system("pause");
+    return 0;
+#endif
+
+    short a; short b;
+    std::cout << "Введите число 1: ";
+    std::cin >> a;
+    std::cout << "Введите число 2: ";
+    std::cin >> b;
+
+#if MODE == 0
+    std::cout << "Вы ввели число 1: " << a << " и число 2: " << b << std::endl;
+#elif MODE == 1
+    std::cout << "Результат сложения: " << add(a,b) << std::endl;
+#endif
+    std::system("pause");
+    return 0;
+}
+#if MODE == 1
+short add(const short& a, const short& b)
+{
+    return a + b;
+}
+#endif
